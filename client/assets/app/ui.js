f1511eaf7490380083926825980d07d403420c0e
Andres <andresdavid@outlook.com>
2016-01-17 18:56:39 -0430

    bug

--- client/assets/app/ui.js
+++ client/assets/app/ui.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
 
     //Accordion Nav
     jQuery('.mainNav').navAccordion({
-      expandButtonText: '<md-buttom data-ng-show="menu.Submenu != 0" class="fa fa-chevron-up" style="color:black"></md-buttom>',  //Text inside of buttons can be HTML
+      expandButtonText: '<md-buttom data-ng-show="menu.Submenu != 0" class=" botup fa fa-chevron-up" style="color:black"></md-buttom>',  //Text inside of buttons can be HTML
       collapseButtonText: '<i data-ng-show="menu.Submenu != 0" class="fa fa-chevron-down" style="color:black"></i>'
     },
     function(){
